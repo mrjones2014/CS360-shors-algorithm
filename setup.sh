0374d6708fa00b2bf86bd822b3160326d870afe5
Mat Jones <mjones@dataforma.com>
2018-04-18 22:42:00 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+python -m pip install --user --global-option=--without-cppsimulator projectq
 pip install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
 jupyter nbextension enable codefolding/main
